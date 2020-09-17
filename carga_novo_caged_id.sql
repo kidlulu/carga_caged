f578f94ce8d3725438c59243360e5bdfb3af9678
kidlulu <luiz.rubens@gmail.com>
2020-09-17 19:47:31 -0300

    Update carga_novo_caged_id.sql

--- carga_novo_caged_id.sql
+++ carga_novo_caged_id.sql
@@ -92,6 +92,18 @@ into caged_id.caged_mov_202001_atual
 from caged_id.tmp
 GO
 
+CREATE NONCLUSTERED INDEX [idx_competencia] ON [caged_id].[caged_mov_202001_atual]
+([competencia] ASC
+)WITH (PAD_INDEX = OFF, 
+       STATISTICS_NORECOMPUTE = OFF, 
+	   SORT_IN_TEMPDB = OFF, 
+	   DROP_EXISTING = OFF, 
+	   ONLINE = OFF, 
+	   ALLOW_ROW_LOCKS = ON, 
+	   ALLOW_PAGE_LOCKS = ON) 
+ON [PRIMARY]
+GO
+
 CREATE NONCLUSTERED INDEX [idx_uf] ON [caged_id].[caged_mov_202001_atual]
 ([uf] ASC
 )WITH (PAD_INDEX = OFF, 
